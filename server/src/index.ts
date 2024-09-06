3580247476e290b9066c04f6bbecd0c16be4fea3
Michael Duisenberg <mrduise@gmail.com>
2024-09-05 21:43:07 -0700

    Fixed caching and added in correct connection to weather api

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,33 +1,44 @@
 import express from 'express';
 import {Request, Response, NextFunction } from 'express';
-import redis  from 'redis';
-import axios from'axios';
+import { createClient } from 'redis';
+import axios from 'axios';
+import * as dotenv from 'dotenv';
 
+dotenv.config();
 
-const client = redis.createClient();
+const client = createClient();
 const app = express();
+app.use(express.json()); // For parsing application/json
 //start the redis client
 client.on('error', (err) => {
   console.error('Redis error:', err);
 });
 
+client.on('connect', () => {
+  console.log('Redis client connected');
+});
 
+
+client.connect();
 //app.use(express.json());
 //
 const fetchWeatherData = async (location: string) => {
     const unitSystem = "us"
-  const url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?unitGroup=${unitSystem}&key=${WEATHER_API_KEY}&contentType=json`
+    "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/Chandler%2C%20Az?unitGroup=us&key=XGQ5CQJVT4T4RGFW5HQQ67R3G&contentType=json"
+  const url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?unitGroup=${unitSystem}&key=${process.env.WEATHER_API_KEY}&contentType=json`
   const response = await axios.get(url);
   return response.data;
 };
 
-app.get('/weather/:location', async (req: Request, res: Response, next: NextFunction) => {
-    const { location } = req.params;
+app.get('/weather', async (req: Request, res: Response, next: NextFunction) => {
+    const { location } = req.body;
+    console.log(location);
+    
   
     try {
         // Check if the data is in Redis
         const cachedData = await client.get(location);
-    
+      
         if (cachedData) {
           // Data found in cache
           console.log('Cache hit');
@@ -44,7 +55,8 @@ app.get('/weather/:location', async (req: Request, res: Response, next: NextFunc
           res.json(weatherData);
         }
       } catch (error) {
-        res.status(500).json({ error: 'An error occurred' });
+        next(error)
+        //res.status(500).json({ error: 'An error occurred' });
       }
     });
 
